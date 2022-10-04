233c6adb8beb43cb63a077f00f11890b1a168d5e
JonatSrG <JonatSrG>
2022-10-03 23:47:10 -0500

    Sw: activate

--- sw.js
+++ sw.js
@@ -2,8 +2,19 @@
 // Ciclo de vida del SW
 
 self.addEventListener('install', event =>{
+    //Descargar Assets
+    //Creamos un cache
+    console.log('Sw: Instalando SW');
 
-    console.log('Instalando SW');
+    //slef.skipWaiting();
 
+});
+
+//Cuando el SW toma el control de la app
+self.addEventListener('activate', event => {
+
+    //Borrar cache viejo
+
+    console.log('Sw2: Activo y listo para controlar la app');
 });
 
